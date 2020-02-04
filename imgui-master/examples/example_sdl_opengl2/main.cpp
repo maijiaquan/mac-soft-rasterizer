731dd282e078568f3d07c0593a3dd00a350ffa63
maijiaquan <499466765@qq.com>
2020-02-05 00:21:13 +0800

    add ray tracing

--- imgui-master/examples/example_sdl_opengl2/main.cpp
+++ imgui-master/examples/example_sdl_opengl2/main.cpp
@@ -18,6 +18,14 @@
 #include "cobloader.h"
 //using namespace std;
 
+
+#include "sphere.h"
+#include "hitable_list.h"
+#include "float.h"
+#include "camera.h"
+#include "material.h"
+#include "random.h"
+#include <thread>
 //全局变量
 enum RenderType
 {
@@ -620,9 +628,125 @@ void DrawDemo9_2()
     }
 }
 
+vec3 color(const ray &r, hitable *world, int depth);
+hitable *random_scene();
+void RayTracing();
+
+vec3 color(const ray& r, hitable *world, int depth) {
+    hit_record rec;
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+        ray scattered;
+        vec3 attenuation;
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+             return attenuation*color(scattered, world, depth+1);
+        }
+        else {
+            return vec3(0,0,0);
+        }
+    }
+    else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5*(unit_direction.y() + 1.0);
+        return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+    }
+}
+
+
+hitable *random_scene() {
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+    list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = random_double();
+            vec3 center(a+0.9*random_double(),0.2,b+0.9*random_double());
+            if ((center-vec3(4,0.2,0)).length() > 0.9) {
+                if (choose_mat < 0.8) {  // diffuse
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new lambertian(vec3(random_double()*random_double(),
+                                            random_double()*random_double(),
+                                            random_double()*random_double()))
+                    );
+                }
+                else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new metal(vec3(0.5*(1 + random_double()),
+                                       0.5*(1 + random_double()),
+                                       0.5*(1 + random_double())),
+                                  0.5*random_double())
+                    );
+                }
+                else {  // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+    return new hitable_list(list,i);
+
+}
+
+
+void RayTracing() {
+    // int nx = 1200;
+    // int ny = 800;
+    int nx = 1000;
+    int ny = 600;
+    int ns = 100;
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+
+    vec3 lookfrom(13,2,3);
+    vec3 lookat(0,0,0);
+    float dist_to_focus = 10.0;
+    float aperture = 0.1;
+    hitable *world = random_scene();
+
+
+    camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
+
+    for (int j = ny-1; j >= 0; j--) {
+        cout<<"j = "<<j<<endl;
+        for (int i = 0; i < nx; i++) {
+                    // cout<<"i = "<<i<<endl;
+
+            vec3 col(0, 0, 0);
+            for (int s=0; s < ns; s++) {
+                // cout<<"s = "<<s<<endl;
+                float u = float(i + random_double()) / float(nx);
+                float v = float(j + random_double()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                col += color(r, world,0);
+            }
+            col /= float(ns);
+            col = vec3( sqrt(col[0]), sqrt(col[1]), sqrt(col[2]) );
+            int ir = int(255.99*col[0]);
+            int ig = int(255.99*col[1]);
+            int ib = int(255.99*col[2]);
+
+
+                    int c;
+                    RGB2Color(c, ir, ig, ib);
+            device_pixel(framebuffer, i, ny-1-j,c ); 
+            // std::cout << "xxx "<< ir << " " << ig << " " << ib << "\n";
+        }
+    }
+}
+
+
 int main(int, char **)
 {
+    // framebuffer = new int[display_w * display_h];
+    framebuffer = new int[1280 * 720]; //todo
 
+    thread t(RayTracing);
+    // RayTracing();
 
     // Setup SDL
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
@@ -755,9 +879,12 @@ int main(int, char **)
         {
             display_w = (int)io.DisplaySize.x;
             display_h = (int)io.DisplaySize.y;
-            framebuffer = new int[display_w * display_h];
             InitDemo9_2();
             gInited = true;
+                    // RayTracing();
+            // thread t(RayTracing);
+            // cout<< display_w <<display_h<<endl;
+
         }
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -771,16 +898,20 @@ int main(int, char **)
             for (int j = 0; j < display_w; j++)
             {
                 // RGB2Color(framebuffer[i * display_w + j], greyLevel * clear_color.x, greyLevel * clear_color.y, greyLevel * clear_color.z);
-                RGB2Color(framebuffer[i * display_w + j], greyLevel * clear_color.x, greyLevel * clear_color.y, greyLevel * clear_color.z);
+                // RGB2Color(framebuffer[i * display_w + j], greyLevel * clear_color.x, greyLevel * clear_color.y, greyLevel * clear_color.z);
             }
         }
-        DrawDemo9_2();
+        // DrawDemo9_2();
         DrawFrame();
+
 
         // glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound
         ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
         SDL_GL_SwapWindow(window);
     }
+
+    // t.join();
+
 
     // Cleanup
     ImGui_ImplOpenGL2_Shutdown();
